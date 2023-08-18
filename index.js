48118ba03d9f0bc8e1fef56af38a46790b091f86
sebastianadams-music <sebastian.adams@hotmail.com>
2023-08-18 11:09:09 +0200

    new pointer function

--- index.js
+++ index.js
@@ -121,3 +121,59 @@ function draw() {
 
     return false
   }
+
+  // deal with pointer
+
+  function init() {
+    // Install event handlers for the pointer target
+    const el = document.getElementById("target");
+    el.onpointerdown = pointerdownHandler;
+    el.onpointermove = pointermoveHandler;
+  
+    // Use same handler for pointer{up,cancel,out,leave} events since
+    // the semantics for these events - in this app - are the same.
+    el.onpointerup = pointerupHandler;
+    el.onpointercancel = pointerupHandler;
+    el.onpointerout = pointerupHandler;
+    el.onpointerleave = pointerupHandler;
+  }  
+
+  function pointermoveHandler(ev) {
+    // This function implements a 2-pointer horizontal pinch/zoom gesture.
+    //
+    // If the distance between the two pointers has increased (zoom in),
+    // the target element's background is changed to "pink" and if the
+    // distance is decreasing (zoom out), the color is changed to "lightblue".
+    //
+    // This function sets the target element's border to "dashed" to visually
+    // indicate the pointer's target received a move event.
+    log("pointerMove", ev);
+    ev.target.style.border = "dashed";
+  
+    // Find this event in the cache and update its record with this event
+    const index = evCache.findIndex(
+      (cachedEv) => cachedEv.pointerId === ev.pointerId,
+    );
+    evCache[index] = ev;
+  
+    // If two pointers are down, check for pinch gestures
+    if (evCache.length === 2) {
+      // Calculate the distance between the two pointers
+      const curDiff = Math.abs(evCache[0].clientX - evCache[1].clientX);
+  
+      if (prevDiff > 0) {
+        if (curDiff > prevDiff) {
+          // The distance between the two pointers has increased
+          zoomCount -= zoomChange
+
+        }
+        if (curDiff < prevDiff) {
+          // The distance between the two pointers has decreased
+          zoomCount += zoomChange
+        }
+      }
+  
+      // Cache the distance for the next move event
+      prevDiff = curDiff;
+    }
+  }
